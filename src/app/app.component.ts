33d96d6559fb1d38a698ec0a0b0a698dcc48eafe
Juan Abreu Rojas <juanluis@Juans-MacBook-Pro.local>
2021-06-29 19:36:09 -0400

    GenexusReportView

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GenexusReportView';
+  myControl = new FormControl();
+  cliente: string[] = ['Juan', 'Miguel', 'Jose','Agustin','Enmanuel','Carlos','Johan','Leandro'];
+  filteredOptions: Observable<string[]>;
+
+
+  constructor(private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('en-GB');
+}
+
+ ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.cliente.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  ShowMessage(mensaje){
+    alert(mensaje);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {  MatSelectModule} from '@angular/material/select';
+import { MatDatepickerModule} from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
